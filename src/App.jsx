630c60543c44045840b7f4b99727ef57b6f7c99d
Arun Karikalan <arun.k@workativ.com>
2025-04-18 09:13:13 +0530

    todo app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 
 function Counter() {
   const [count, setCount] = useState(0);
@@ -22,33 +23,75 @@ function MySecond() {
   );
 }
 
+function TodoAdder({ added }) {
+  const [typedTodo, setTypedTodo] = useState("");
+  return (
+    <div>
+      <input
+        value={typedTodo}
+        onChange={(e) => setTypedTodo(e.target.value)}
+      ></input>
+      <button
+        onClick={() => {
+          added(typedTodo);
+          setTypedTodo("");
+        }}
+      >
+        Add
+      </button>
+    </div>
+  );
+}
+
+function TodoItem({ completed, todo, remove }) {
+  return (
+    <div>
+      <input type="checkbox" checked={completed}></input>
+      <span>{todo}</span>
+      <button onClick={() => remove()}>remove</button>
+    </div>
+  );
+}
+
+function TodoList({ todos, remove }) {
+  return (
+    <div>
+      {/* <TodoItem todo={"prepare for react class"} completed={true}></TodoItem> */}
+      {todos.map(({ completed, todo, id }) => (
+        <TodoItem
+          todo={todo}
+          completed={completed}
+          key={id}
+          remove={() => remove(id)}
+        ></TodoItem>
+      ))}
+    </div>
+  );
+}
+
 function App() {
-  // React.Fragment();
-  let [numOfCounters, setNumOfCounters] = useState(1);
-  let counters = [];
-  for (let i = 0; i < numOfCounters; i++) {
-    counters.push(<Counter key={i} />);
-  }
+  let initialTodo = [
+    { id: uuidv4(), completed: false, todo: "give summary of a class" },
+  ];
+  const [todos, setTodos] = useState(initialTodo);
+
   return (
     <>
-      <button onClick={() => setNumOfCounters(numOfCounters + 1)}>+</button>
-      {counters}
+      <TodoAdder
+        added={(todo) => {
+          console.log({ todo });
+          setTodos([...todos, { id: uuidv4(), completed: false, todo }]);
+        }}
+      />
+      <TodoList
+        todos={todos}
+        remove={(idToRemove) => {
+          setTodos(todos.filter(({ id }) => id !== idToRemove));
+        }}
+      />
+      {/* {TodoList({todos, remove})} */}
     </>
   );
-
-  // return (
-  //   <>
-  //     {/* <MyComponent name="vivek" /> */}
-  //     {counters}
-  //   </>
-  // );
-  // return [<MyComponent></MyComponent>, <MySecond></MySecond>]
-  // return React.createElement(
-  //   React.Fragment,
-  //   null,
-  //   React.createElement(MyComponent),
-  //   React.createElement(MySecond)
-  // );
 }
 
 // undefined, null, jsx - dom element, another component, any valid js value
